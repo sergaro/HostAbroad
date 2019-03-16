1ad57ddb7293d0df00ca3d56ab2431e566bb020c
GasanNazer <saninazer@gmail.com>
2019-03-16 23:04:17 +0100

    Created TUser and User Entity

---
+++ HostAbroad/src/main/java/com/business/TUser.java
@@ -0,0 +1,55 @@
+package business.search;
+
+public class TUser {
+	private String nickname;
+	private double rating;
+	private String description;
+	private boolean host;
+	
+	public TUser() {};
+	
+	public TUser(String nickname, double rating, String description, boolean host){
+		this.nickname = nickname;
+		this.rating = rating;
+		this.description = description;
+		this.host = host;
+	}
+	
+	public TUser(String nickname, double rating, String description){
+		this.nickname = nickname;
+		this.rating = rating;
+		this.description = description;
+	}
+	
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public String getNickName() {
+		return this.nickname;
+	}
+	
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
+	public double getRating() {
+		return this.rating;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public void setHost(boolean host) {
+		this.host = host;
+	}
+	
+	public boolean getHost() {
+		return this.host;
+	}
+}
---
+++ HostAbroad/src/main/java/com/business/User.java
@@ -0,0 +1,63 @@
+package business.search;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class User {
+	
+	@Id
+	private String nickname;
+	private double rating;
+	private String description;
+	private boolean host;
+	
+	public User() {};
+	
+	public User(String nickname, double rating, String description, boolean host){
+		this.nickname = nickname;
+		this.rating = rating;
+		this.description = description;
+		this.host = host;
+	}
+	
+	public User(String nickname, double rating, String description){
+		this.nickname = nickname;
+		this.rating = rating;
+		this.description = description;
+	}
+	
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public String getNickName() {
+		return this.nickname;
+	}
+	
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
+	public double getRating() {
+		return this.rating;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public void setHost(boolean host) {
+		this.host = host;
+	}
+	
+	public boolean getHost() {
+		return this.host;
+	}
+}
