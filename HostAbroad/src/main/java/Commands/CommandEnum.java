18461083c723a3425f94d228dbe7b9ec2200f689
ivanalba1 <33236797+ivanalba1@users.noreply.github.com>
2019-03-17 13:37:52 +0100

    Comments on classes

--- HostAbroad/src/main/java/Commands/CommandEnum.java
+++ HostAbroad/src/main/java/Commands/CommandEnum.java
@@ -2,6 +2,7 @@ package Commands;
 
 public class CommandEnum {
 
+	//Clase enum para hacer seleccion dinámica de comando con todos los que usaremos 
 	public enum Commands {
 	    COMMAND_SEARCH_HOST
 	}
--- HostAbroad/src/main/java/com/presentation/Command.java
+++ HostAbroad/src/main/java/com/presentation/Command.java
@@ -2,6 +2,7 @@ package com.presentation;
 
 public abstract class Command {
 
+	//Se modificara segun el Integer del pair para actualizar la vista
 	private int eventReturn;
 	
 	//Execute devolvera un Pair con un Integer(0 si hay error, 1 si no hay error) y el transfer para
