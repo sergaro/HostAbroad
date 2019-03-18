2f3e2ee60e6de5a18e2b0951f9516e2b7a356264
sergaro <48321722+sergaro@users.noreply.github.com>
2019-03-18 17:11:01 +0100

    Update ASFactoryImp.java
    Removed empty line

--- HostAbroad/src/main/java/com/business/ASFactory/ASFactoryImp.java
+++ HostAbroad/src/main/java/com/business/ASFactory/ASFactoryImp.java
@@ -3,8 +3,7 @@ package com.business.ASFactory;
 import com.business.ASSearch.ASSearch;
 import com.business.ASSearch.ASSearchImp;
 
-public class ASFactoryImp extends ASFactory {
-	
+public class ASFactoryImp extends ASFactory {	
 	
 	public ASSearch createASSearch() {
 		return new ASSearchImp();
